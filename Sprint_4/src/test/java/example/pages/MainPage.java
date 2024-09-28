07482c400771dbe4a9fe4b219ecdb433298ff264
kirillsnep <kirill.snep@yandex.ru>
2024-09-29 01:05:34 +0300

    Add files via upload

---
+++ Sprint_4/src/test/java/example/pages/MainPage.java
@@ -0,0 +1,25 @@
+package example.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class MainPage {
+    public MainPage(WebDriver driver) {
+
+    }
+
+    public void clickOrderButtonTop() {
+    }
+
+    public void fillOrderForm(String name, String address) {
+    }
+
+    public void submitOrder() {
+    }
+
+    public boolean isSuccessMessageDisplayed() {
+        return true;
+    }
+
+    public void clickOrderButtonBottom() {
+    }
+}
